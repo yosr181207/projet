4fa76f03e4c9d066ebcc08aba0a933439f459a7d
yosr181207 <ghargharyosr@gmail.com>
2024-06-28 13:48:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.currentQuestionIndex = 0;
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getCurrentQuestion().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+    this.currentQuestionIndex++;
+};
+
+Quiz.prototype.getCurrentQuestion = function() {
+    return this.questions[this.currentQuestionIndex];
+};
+
+Quiz.prototype.hasEnded = function() {
+    return this.currentQuestionIndex >= this.questions.length;
+};
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function (choice) {
+    return this.answer === choice;
+};
+var QuizUI = {
+    displayNext: function () {
+        if (quiz.hasEnded()) {
+            this.displayScore();
+        } else {
+            this.displayQuestion();
+            this.displayChoices();
+            this.displayProgress();
+        }
+    },
+    displayQuestion: function() {
+        this.populateIdWithHTML("question", quiz.getCurrentQuestion().text);
+    },
+    displayChoices: function() {
+        var choices = quiz.getCurrentQuestion().choices;
+
+        for(var i = 0; i < choices.length; i++) {
+            this.populateIdWithHTML("choice" + i, choices[i]);
+            this.guessHandler("guess" + i, choices[i]);
+        }
+    },
+    displayScore: function() {
+        var gameOverHTML = "<h1>Game Over</h1>" ;
+        gameOverHTML += "<h2> Your score is: " + quiz.score + "</h2>";
+        this.populateIdWithHTML("quiz", gameOverHTML);
+    },
+    
+    populateIdWithHTML: function(id, text) {
+        var element = document.getElementById(id);
+        element.innerHTML = text;
+    },
+    guessHandler: function(id, guess) {
+        var button = document.getElementById(id);
+        button.onclick = function() {
+            quiz.guess(guess);
+            QuizUI.displayNext();
+        }
+    },
+    
+    displayProgress: function() {
+        var currentQuestionNumber = quiz.currentQuestionIndex + 1;
+        this.populateIdWithHTML("progress", "Question " + currentQuestionNumber + " of " + quiz.questions.length);
+    }
+};
+//Create Questions
+var questions = [
+    new Question(" What year did Arnold Schwarzenegger first win the Mr. Olympia title?", [ "1980", "1987", "1970", "I don't know" ], "1970"),
+    new Question(" How many Mr.Olympia titles did Lee Haney win during his career?", ["2","8", "4", "I don't know"], "8"),
+    new Question("Do you love anas&yosr?", ["No","Yes", "Hell Yeah"], "Hell Yeah"),
+    new Question("Which type of muscle fiber is associated with short, powerful movements?", ["slow-twitch","fast-twitch", "medium-twitch"], "fast-twitch"),
+    new Question("Who was the oldest man to compete in the Mr. Olympia?", ["chris","Shawn Rhoden","Albert Beckles"], "Albert Beckles")
+    
+];
+
+//Create Quiz
+var quiz = new Quiz(questions);
+
+//Display Quiz
+QuizUI.displayNext();
